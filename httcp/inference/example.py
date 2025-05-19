8a4cf1e0160689905a6d0c690d880f67a11cfcd0
Aliya Nigamova <aliya.nigamova@cern.ch>
2025-05-19 17:15:33 +0200

    first version of datacards production, just 2 categories

--- httcp/inference/example.py
+++ httcp/inference/example.py
@@ -3,8 +3,11 @@
 """
 Example inference model.
 """
+import functools 
 
 from columnflow.inference import inference_model, ParameterType, ParameterTransformation
+from columnflow.config_util import get_datasets_from_process 
+
 
 
 @inference_model
@@ -12,47 +15,72 @@ def example(self):
 
     #
     # categories
-    #
+    
 
     self.add_category(
-        "cat1",
-        config_category="incl",
-        config_variable="jet1_pt",
-        config_data_datasets=["data_mu_b"],
-        mc_stats=True,
-    )
-    self.add_category(
-        "cat2",
-        config_category="2j",
-        config_variable="jet1_eta",
-        # fake data from TT
-        data_from_processes=["TT"],
+        "cat_mutau_sr",
+        config_category="cat_mutau_sr",
+        config_variable="phi_cp_mu_pi",
+        config_data_datasets=["data_singlemu_C", "data_mu_C", "data_mu_D"],
         mc_stats=True,
     )
 
-    #
-    # processes
-    #
+   
+    self.add_category(
+        "cat_mutau_abcd_ar",
+        config_category="cat_mutau_abcd_ar",
+        config_variable="mutau_mt",
+        config_data_datasets=["data_singlemu_C", "data_mu_C", "data_mu_D"],
+        mc_stats=True,
+    )
 
-    self.add_process(
-        "ST",
-        is_signal=True,
-        config_process="st",
-        config_mc_datasets=["st_tchannel_t_powheg"],
-    )
-    self.add_process(
-        "TT",
-        config_process="tt",
-        config_mc_datasets=["tt_sl_powheg"],
-    )
+    
+    # processes and datasets
+
+    process_vs_dataset_names = {
+        "data": ["data_singlemu_C", "data_mu_C", "data_mu_D"],       
+        
+        #Drell-Yan
+        "dy_z2ee": ["dy_lep_madgraph"],
+        "dy_z2mumu": ["dy_lep_madgraph"],
+        "dy_z2tautau": ["dy_lep_madgraph"],
+  
+        "wj": ["wj_incl_madgraph"],
+        #diboson
+        "vv": ["ww", "wz", "zz"], #diboson inclusive
+        #ttbar
+        "tt": ["tt_sl","tt_dl","tt_fh"], #ttbar inclusive
+        #single top
+        "st": ["st_twchannel_t_sl", "st_twchannel_tbar_sl", "st_twchannel_tbar_dl", "st_tchannel_tbar", "st_tchannel_t", "st_schannel_t_lep", "st_schannel_tbar_lep"], #single top inclusive
+        #signal
+        "h_ggf_htt": ["h_ggf_htt_filtered"], #SM Higgs signal
+    }
+ 
+    find_datasets = functools.partial(get_datasets_from_process, self.config_inst, strategy="all")
+
+    for process_name, dataset_names in process_vs_dataset_names.items():
+
+        is_signal = False
+        dataset_names_tmp = [dataset.name for dataset in find_datasets(process_name)]
+        print(process_name, dataset_names_tmp)
+        
+        if process_name == "h_ggf_htt": 
+            is_signal = True
+ 
+        self.add_process(
+            process_name,
+            config_process=process_name,
+            config_mc_datasets=dataset_names,
+            is_signal=is_signal,
+        )
 
     #
     # parameters
     #
 
     # groups
-    self.add_parameter_group("experiment")
-    self.add_parameter_group("theory")
+ #   self.add_parameter_group("experiment")
+ #   self.add_parameter_group("theory")
 
     # lumi
     lumi = self.config_inst.x.luminosity
@@ -63,39 +91,6 @@ def example(self):
             effect=lumi.get(names=unc_name, direction=("down", "up"), factor=True),
             transformations=[ParameterTransformation.symmetrize],
         )
-
-    # tune uncertainty
-    self.add_parameter(
-        "tune",
-        process="TT",
-        type=ParameterType.shape,
-        config_shift_source="tune",
-    )
-
-    # muon weight uncertainty
-    self.add_parameter(
-        "mu",
-        process=["ST", "TT"],
-        type=ParameterType.shape,
-        config_shift_source="mu",
-    )
-
-    # jet energy correction uncertainty
-    self.add_parameter(
-        "jec",
-        process=["ST", "TT"],
-        type=ParameterType.shape,
-        config_shift_source="jec",
-    )
-
-    # a custom asymmetric uncertainty that is converted from rate to shape
-    self.add_parameter(
-        "QCDscale_ttbar",
-        process="TT",
-        type=ParameterType.shape,
-        transformations=[ParameterTransformation.effect_from_rate],
-        effect=(0.5, 1.1),
-    )
 
 
 @inference_model
---
+++ scripts/05_create_datacards.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+source ./common_run3.sh #to access set_common_vars() function
+#The following function defines config, processes, version and datasets variables
+set_common_vars "$1"
+args=(
+        --config $config
+
+        --cf.CalibrateEvents-workflow $workflow
+        --cf.CalibrateEvents-version $version
+        --version $version
+        --cf.SelectEvents-workflow $workflow
+        --cf.SelectEvents-version $version
+        
+        --cf.MergeReducedEvents-workflow $workflow
+        --cf.MergeReducedEvents-version $version
+        
+        --cf.MergeSelectionStats-version $version
+        --cf.ProvideReducedEvents-version $version
+        --inference-model example
+        "${@:2}"
+    )
+echo law run cf.CreateDatacards "${args[@]}"
+law run cf.CreateDatacards "${args[@]}"
